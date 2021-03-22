9214a90500d34ed8733b47e0493aa1d90270538b
Rémy Beumier <beumieremy@gmail.com>
2021-03-22 12:18:08 +0100

    difficulty options added

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// reset button?
-// add easy and hard options? (n3 and n9)
 
-var n = 6;
-var colorsBlocks = document.querySelectorAll(".colors__block");
+// reset button?
+// add Hue and Hex options
+// dark theme?
+
+var diffEls = document.querySelectorAll(".diff__btn");
+var diffEl = document.querySelector(".diff__btn.active").innerHTML;
+var n = diffEl;
+var colorsEl = document.querySelector(".colors");
+var colorsBlocks;
 var rgbEl = document.querySelector(".rgb");
 var statusEl = document.querySelector(".status");
 var colors = [];
+createBlocks(n);
 resetGame();
-
-for (var i=0; i<colorsBlocks.length; i++) {
-  colorsBlocks[i].addEventListener("click", checkColors);
-}
 
 function checkColors(e) {
   if (colors[pickedColor] === e.target.style.backgroundColor) {
@@ -29,6 +31,7 @@ function checkColors(e) {
 
 function resetGame() {
   setTimeout(function() {
+    createBlocks(n);
     document.body.style.color = "black";
     colors = [];
     pickColors();
@@ -56,5 +59,32 @@ function randomColor() {
 }
 
 function random(r) {
-  return Math.floor(Math.random()*r)
+  return Math.floor(Math.random()*r);
+}
+
+for (var i=0; i<diffEls.length; i++) {
+  diffEls[i].addEventListener("click", setN);
+}
+
+function setN(e) {
+  for (var i=0; i<diffEls.length; i++) {
+    diffEls[i].classList.remove("active");
+  }
+  e.target.classList.add("active");
+  diffEl = document.querySelector(".diff__btn.active").innerHTML;
+  n = diffEl;
+  resetGame();
+}
+
+function createBlocks(num) {
+  colorsEl.innerHTML = "";
+  for (var i=0; i<num; i++) {
+    var block = document.createElement("div");
+    block.classList.add("colors__block");
+    colorsEl.appendChild(block);
+  }
+  colorsBlocks = document.querySelectorAll(".colors__block");
+  for (var i=0; i<colorsBlocks.length; i++) {
+    colorsBlocks[i].addEventListener("click", checkColors);
+  }
 }
